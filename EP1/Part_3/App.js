1c5428a163f8be34a056dc780fe6e9872c08404a
abhishek <abhishekudiya09@gmail.com>
2023-11-06 01:34:34 +0530

    Episode 2 content

--- EP1/Part_3/App.js
+++ EP1/Part_3/App.js
@@ -2,5 +2,5 @@ const header = React.createElement("h1",
 {id :"text"},
 "Hello World"
 )
-const root = React.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(header);
